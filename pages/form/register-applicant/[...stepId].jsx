949dc9b01d7bb31cce4cde2cab02eb4eab591fcc
OliZor <olivia.zorn@softwire.com>
2021-06-20 23:33:21 +0100

    formatted date with package and made pdf component

--- pages/form/register-applicant/[...stepId].jsx
+++ pages/form/register-applicant/[...stepId].jsx
@@ -2,21 +2,12 @@ import React, { useState } from 'react';
 import uuid from "react-uuid";
 import FormWizard from "components/FormWizard/FormWizard";
 import form from "data/forms/register-applicant";
-import { Document, Page, pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import { format } from 'date-fns';
+import FileViewer from 'components/FileViewer/FileViewer';
 
 const RegisterApplicant = () => {
   const currentDateTime = () => {
-    var today = new Date();
-    var date =
-      today.getFullYear() +
-      "-" +
-      (today.getMonth() + 1) +
-      "-" +
-      today.getDate();
-    var time =
-      today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    return date + " " + time;
+    return format(new Date(), "yyyy-MM-dd' 'HH:mm:ss");
   };
 
   const processFormData = async (formData) => {
@@ -25,24 +16,6 @@ const RegisterApplicant = () => {
   };
 
   const onFormSubmit = (formData) => processFormData(formData);
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-
-  const nextPage = () => {
-    if (pageNumber < numPages) {
-      setPageNumber(pageNumber + 1)
-    }
-  }
-
-  const prevPage = () => {
-    if (pageNumber > 1) {
-      setPageNumber(pageNumber - 1)
-    }
-  }
 
   return (
     <div id="columns">
@@ -61,31 +34,7 @@ const RegisterApplicant = () => {
           />
         </div>
         <div className="govuk-grid-column-one-half reference-image sticky">
-          <Document
-            file="https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/931882/Register-to-vote-if-youre-living-in-England.pdf"
-            onLoadSuccess={onDocumentLoadSuccess}
-          >
-            <Page pageNumber={pageNumber} />
-          </Document>
-          <div className="page-controls lbh-simple-pagination govuk-!-margin-top-3">
-            <button 
-              className="lbh-simple-pagination__link" 
-              onClick={prevPage} 
-            >
-              <svg width="11" height="19" viewBox="0 0 11 19" fill="none">
-                <path d="M10 1L2 9.5L10 18" stroke-width="2" />
-              </svg>
-            </button>
-            <p className="lbh-simple-pagination__link page-indicator">{pageNumber} of {numPages}</p>
-            <button
-              className="lbh-simple-pagination__link lbh-simple-pagination__link--next"
-              onClick={nextPage}
-            >
-              <svg width="11" height="19" viewBox="0 0 11 19" fill="none">
-                <path d="M1 18L9 9.5L1 1" stroke-width="2" />
-              </svg>
-            </button>
-          </div>
+          <FileViewer file={{ url: "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/931882/Register-to-vote-if-youre-living-in-England.pdf" }} />
         </div>
       </div>
     </div>
