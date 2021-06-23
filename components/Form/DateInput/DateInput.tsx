e592b814784ed14171ecb8e684d0c20e1676b6a7
OliZor <olivia.zorn@softwire.com>
2021-06-23 14:47:52 +0100

    changed react-hook-form to version 6

--- components/Form/DateInput/DateInput.tsx
+++ components/Form/DateInput/DateInput.tsx
@@ -159,7 +159,7 @@ const ControlledDateInput = ({
   const inputRef = useRef<HTMLInputElement>(null);
   return (
     <Controller
-      render={({ field: { onChange, value } }) => (
+      render={({ onChange, value }) => (
         <DateInput
           name={name}
           value={value}
@@ -170,7 +170,7 @@ const ControlledDateInput = ({
         />
       )}
       name={name}
-      // onFocus={() => inputRef.current?.focus()}
+      onFocus={() => inputRef.current?.focus()}
       rules={{
         ...rules,
         validate: {
