b86ea6236be41d6dfbdd7c4c1b24792aa0da1d95
OliZor <olivia.zorn@softwire.com>
2021-06-25 14:55:03 +0100

    changed date formatting expectation in spec

--- utils/date.spec.ts
+++ utils/date.spec.ts
@@ -22,8 +22,8 @@ describe('date util', () => {
 
   describe('formatDate', () => {
     it('should work properly', () => {
-      expect(formatDate('22/09/1941')).toEqual('22 Sep 1941');
-      expect(formatDate('22/09/1941 13:49:43')).toEqual('22 Sep 1941');
+      expect(formatDate('22/09/1941')).toEqual('22 Sept 1941');
+      expect(formatDate('22/09/1941 13:49:43')).toEqual('22 Sept 1941');
     });
   });
 
