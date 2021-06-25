be71415cf8ea2fec46a3291554c2a4efdca2c328
OliZor <olivia.zorn@softwire.com>
2021-06-25 12:34:14 +0100

    connected stylesheet and simplified layout

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,9 +1,5 @@
 import Seo from './Seo/Seo';
 import Header from './Header/Header';
-import SkipLink from './SkipLink/SkipLink';
-import PhaseBanner from './PhaseBanner/PhaseBanner';
-import BackButton from './BackButton/BackButton';
-import Footer from './Footer/Footer';
 
 export interface Props {
   children: React.ReactChild;
@@ -11,22 +7,15 @@ export interface Props {
 }
 
 const Layout = ({ goBackButton, children }: Props): React.ReactElement => {
-  const feedbackLink = process.env.NEXT_PUBLIC_FEEDBACK_LINK || '';
   return (
     <>
-      <Seo title="Social Care Admin - Hackney Council" />
-      <SkipLink />
-      <Header serviceName="Social Care" />
-      <PhaseBanner phase="beta" feedbackLink={feedbackLink} />
-      {goBackButton && <BackButton />}
-
+      <Seo title="Register Admin - Hackney Council" />
+      <Header serviceName="REGISTER" />
       <div className="govuk-width-container">
         <main className="govuk-main-wrapper" id="content" role="main">
           {children}
         </main>
       </div>
-
-      <Footer />
     </>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+import 'stylesheets/all.scss';
+import 'stylesheets/header.scss';
+import Layout from 'components/Layout';
+
+
+
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return(
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  ); 
 }
 export default MyApp;
