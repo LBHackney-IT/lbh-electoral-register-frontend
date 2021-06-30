829bc5a1ae04402c721d83394f03da6160a9d8a4
OliZor <olivia.zorn@softwire.com>
2021-06-30 13:33:31 +0100

    added logic to check elector id is valid

--- pages/review-application.tsx
+++ pages/review-application.tsx
@@ -4,11 +4,25 @@ import { electors } from 'data/electors';
 import SideNav from 'components/SideNav/SideNav';
 import ReviewApplicationTable from 'components/Tables/ReviewApplicationTable';
 import LinkButton from 'components/LinkButton/LinkButton';
+import ErrorMessage from 'components/ErrorMessage/ErrorMessage';
 
 const ReviewApplication = (): React.ReactElement => {
   const router = useRouter();
-  const electorId = parseInt(router.query.electorId);
-  const elector = electors[electorId];
+
+  const displayTable = () => {
+    if (typeof router.query.electorId === 'string') {
+      const electorId = parseInt(router.query.electorId);
+      const elector = electors[electorId];
+      return <ReviewApplicationTable elector={elector} />;
+    } else {
+      return (
+        <ErrorMessage
+          label="Error: Elector ID is invalid."
+          className="govuk-!-margin-top-3"
+        />
+      );
+    }
+  };
 
   return (
     <div className="govuk-grid-row">
@@ -19,7 +33,7 @@ const ReviewApplication = (): React.ReactElement => {
         <h2 className="lbh-header__title govuk-!-margin-bottom-7">
           Review application
         </h2>
-        <ReviewApplicationTable elector={elector} />
+        {displayTable()}
         <LinkButton
           label="Add to register"
           route="/verified-applications"
