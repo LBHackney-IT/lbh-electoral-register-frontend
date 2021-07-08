616062cb927d9b86af93bed9c878789ad1ac3fcc
OliZor <olivia.zorn@softwire.com>
2021-07-08 09:46:31 +0100

    removed unecessary if statement

--- pages/edit-elector/open-register/[...electorId].tsx
+++ pages/edit-elector/open-register/[...electorId].tsx
@@ -26,50 +26,47 @@ const EditOpenRegisterPreference = (): React.ReactElement => {
   };
 
   const displayForm = () => {
-    if (electorId) {
-      const elector = electors.find((elector) => elector.urn === electorId[0]);
-      if (elector) {
-        return (
-          <div>
-            <h2 className="lbh-header__title govuk-!-margin-bottom-7">
-              Open register preference
-            </h2>
-            <form onSubmit={onFormSubmit}>
-              <Radios
-                name="open_register"
-                label="Does the elector want to be included in the open register?"
-                options={['Yes', 'No']}
-                required
+    if (elector) {
+      return (
+        <div>
+          <h2 className="lbh-header__title govuk-!-margin-bottom-7">
+            Open register preference
+          </h2>
+          <form onSubmit={onFormSubmit}>
+            <Radios
+              name="open_register"
+              label="Does the elector want to be included in the open register?"
+              options={['Yes', 'No']}
+              required
+            />
+            <div>
+              <LinkButton
+                label="Save Changes"
+                route={`/view-elector/${elector.urn}`}
+                query={{
+                  electorId: elector.urn,
+                  status: 'success',
+                  edit: 'open-register',
+                }}
+                className="govuk-!-margin-right-1"
+              ></LinkButton>
+              <Button
+                isSecondary
+                label="Cancel"
+                type="button"
+                onClick={onCancel}
               />
-              <div>
-                <LinkButton
-                  label="Save Changes"
-                  route={`/view-elector/${elector.urn}`}
-                  query={{
-                    electorId: elector.urn,
-                    status: 'success',
-                    edit: 'open-register',
-                  }}
-                  className="govuk-!-margin-right-1"
-                ></LinkButton>
-                <Button
-                  isSecondary
-                  label="Cancel"
-                  type="button"
-                  onClick={onCancel}
-                />
-              </div>
-            </form>
-          </div>
-        );
-      } else {
-        return (
-          <ErrorMessage
-            label="Error: Elector ID is invalid."
-            className="govuk-!-margin-top-3"
-          />
-        );
-      }
+            </div>
+          </form>
+        </div>
+      );
+    } else {
+      return (
+        <ErrorMessage
+          label="Error: Elector ID is invalid."
+          className="govuk-!-margin-top-3"
+        />
+      );
     }
   };
 
