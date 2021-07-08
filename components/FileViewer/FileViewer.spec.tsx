278d26a2688e64c219c21bad98b282ff043c3e42
OliZor <olivia.zorn@softwire.com>
2021-07-08 12:24:08 +0100

    renamed PdfViewer to FileViewer and added test

---
+++ components/FileViewer/FileViewer.spec.tsx
@@ -0,0 +1,27 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import FileViewer from './FileViewer';
+
+describe('FileViewer', () => {
+  const file = {
+    url: 'https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/931882/Register-to-vote-if-youre-living-in-England.pdf',
+  };
+
+  it('displays file', () => {
+    render(<FileViewer file={file}></FileViewer>);
+    expect(
+      screen.getByText('Failed to load PDF file.')
+    ).not.toBeInTheDocument();
+  });
+
+  it('renders page numbers', () => {
+    render(<FileViewer file={file}></FileViewer>);
+    expect(screen.getByText('1 of 3')).toBeInTheDocument();
+  });
+
+  it('changes page on click', () => {
+    render(<FileViewer file={file}></FileViewer>);
+    fireEvent.click(screen.getByTestId('next-button'));
+    expect(screen.getByText('2 of 3')).toBeInTheDocument();
+  });
+});
