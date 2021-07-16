aef795c345e523ecb02e4ce0646fc180ab8743d0
OliZor <olivia.zorn@softwire.com>
2021-07-16 11:09:31 +0100

    turned confirmationBanner test into a snapshot

--- components/ConfirmationBanner/ConfirmationBanner.spec.tsx
+++ components/ConfirmationBanner/ConfirmationBanner.spec.tsx
@@ -3,18 +3,13 @@ import { render, screen } from '@testing-library/react';
 import ConfirmationBanner from './ConfirmationBanner';
 
 describe('ConfirmationBanner', () => {
-  it('renders title', () => {
-    render(<ConfirmationBanner title="This is the title"></ConfirmationBanner>);
-    expect(screen.getByText('This is the title'));
-  });
-
-  it('renders content', () => {
-    render(
+  it('should title and content properly', () => {
+    const { asFragment } = render(
       <ConfirmationBanner
         title="This is the title"
         content="This is the content"
       ></ConfirmationBanner>
     );
-    expect(screen.getByText('This is the content'));
+    expect(asFragment()).toMatchSnapshot();
   });
 });
